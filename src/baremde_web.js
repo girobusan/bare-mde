370bbef9e495df7902f173bb444a0f9227494ad4
Gleb T <girobusan@>
2024-03-21 12:03:34 +0300

    start web version

---
+++ src/baremde_web.js
@@ -0,0 +1,8 @@
+console.log("BareMDE web version");
+import {h, render } from "preact";
+import { BareMDE } from "./components/BareMDE";
+
+window.BareMDE = function( props , element ){
+  const e = h(BareMDE , props  );
+  render( e , element )
+}
